fee693e52fcc6aa1ea72dd21d7fc5004fb32f217
huseyinbagator <bagatorhuseyin0@gmail.com>
2023-10-21 16:17:55 +0300

    Creating theme slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import weatherReducer from "./features/weatherSlice";
+import themeReducer from "./features/themeSlice.js";
 
 const reducer = {
   weather: weatherReducer,
+  theme: themeReducer,
 };
 
 export const store = configureStore({
