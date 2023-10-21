c73dc4d089b9b733eeb09914044d5f2ed0a14bc6
huseyinbagator <bagatorhuseyin0@gmail.com>
2023-10-21 11:09:50 +0300

    rejected and pending

--- src/store/features/weatherSlice.js
+++ src/store/features/weatherSlice.js
@@ -3,6 +3,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import getCityWeather from "../../api/weather";
 
 const initialState = {
+  name: "",
   main: {
     feels_like: 0,
   },
@@ -24,13 +25,28 @@ const weatherSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getWeather.fulfilled, (state, action) => {
-      state.name = action.payload.name;
-      state.main.feels_like = action.payload.main.feels_like;
-      state.weather[0].main = action.payload.weather[0].main;
-      state.weather[0].description = action.payload.weather[0].description;
-      state.wind.speed = action.payload.wind.speed;
-    });
+    builder
+      .addCase(getWeather.fulfilled, (state, action) => {
+        state.name = action.payload.name;
+        state.main.feels_like = action.payload.main.feels_like;
+        state.weather[0].main = action.payload.weather[0].main;
+        state.weather[0].description = action.payload.weather[0].description;
+        state.wind.speed = action.payload.wind.speed;
+      })
+      .addCase(getWeather.rejected, (state, action) => {
+        state.name = "City not found";
+        state.main.feels_like = 0;
+        state.weather[0].main = "";
+        state.weather[0].description = "";
+        state.wind.speed = 0;
+      })
+      .addCase(getWeather.pending, (state, action) => {
+        state.name = "Loading...";
+        state.main.feels_like = 0;
+        state.weather[0].main = "";
+        state.weather[0].description = "";
+        state.wind.speed = 0;
+      });
   },
 });
 
